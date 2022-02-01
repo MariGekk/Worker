3895a636c8a59b00a7c9050674b2f65bd2fc93ee
Marina <lirika26.07@rambler.ru>
2022-02-01 21:51:18 +0300

    second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,13 @@
 package com.company;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) {
         Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener listener2 = System.out::println;
 
-
-        Worker worker = new Worker(listener);
+        Worker worker = new Worker(listener, listener2);
         worker.start();
-
     }
 }
+
+
--- src/com/company/Worker.java
+++ src/com/company/Worker.java
@@ -1,11 +1,27 @@
 package com.company;
 
+import java.util.List;
+
 public class Worker {
 
     private OnTaskDoneListener callback;
     private OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener listener) {
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i < 67) {
+                callback.onDone("Task " + i + " is done");
+            } else {
+                errorCallback.onError("Task " + i + " is not done");
+            }
+        }
     }
 
 
@@ -17,16 +33,6 @@ public class Worker {
     @FunctionalInterface
     public interface OnTaskErrorListener {
         void onError(String result);
-    }
-
-    public void start() {
-        for (int i = 0; i < 100; i++) {
-            if (i < 33) {
-                errorCallback.onError("Task " + i + " is not done");
-            } else {
-                callback.onDone("Task " + i + " is done");
-            }
-        }
     }
 
 
